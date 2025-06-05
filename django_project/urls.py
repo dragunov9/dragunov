7c49c49d38c97d9d8a3c2cf5dc534ab7a4b285e6
dragunov9 <alialialoow@gmail.com>
2025-06-06 02:53:33 +0300

    update

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -56,5 +58,4 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
