c99ff45ca9f62081b447844e9276080f6fca0d11
dragunov9 <alialialoow@gmail.com>
2025-06-06 21:42:57 +0300

    update

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.URLField(max_length=500, blank=True, null=True) 
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
 
     def __str__(self):
         return f'{self.user.username} Profile'
