b3b44ae894139062bb4eda84c9765e5170cbb660
dragunov9 <alialialoow@gmail.com>
2025-06-07 04:08:57 +0300

    update

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .imagekit_utils import upload_image
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,11 +11,14 @@ class Profile(models.Model):
     )
 
     def save(self, *args, **kwargs):
-        if self.image and not str(self.image).startswith('http') and 'default.jpg' not in str(self.image):
-            file_data = self.image.read()
-            upload_response = upload_image(file_data, self.image.name)
-            if upload_response and upload_response.response and upload_response.response.url:
-                self.image = upload_response.response.url
+        
+        if self.image and not str(self.image).startswith('http') and os.path.exists(self.image.path):
+            with open(self.image.path, 'rb') as f:
+                file_data = f.read()
+                upload_response = upload_image(file_data, self.image.name)
+                if upload_response.response and upload_response.response.url:
+                    self.image = upload_response.response.url
+
         super().save(*args, **kwargs)
 
     def __str__(self):
